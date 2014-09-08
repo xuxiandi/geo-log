a246e2bcd5b186989f93e82a43488d981ee6391a
alxponom <alxponom@mail.ru>
2014-09-08 06:02:29 +0000

    A file streaming fix.

--- src/com/geoscope/GeoLog/DEVICEModule/TDEVICEModule.java
+++ src/com/geoscope/GeoLog/DEVICEModule/TDEVICEModule.java
@@ -1330,6 +1330,7 @@ public class TDEVICEModule extends TModule
 						flDisconnect = false;
 						throw new StreamingErrorException(Descriptor); //. =>
 					}
+					DEVICEModule.Log.WriteWarning("DEVICEModule.ComponentFileStreaming","Error of streaming the file: "+Item.FileName+", unexpected EOF, restarting it again ...");
 					return; //. ->
 				}
 				//. send file size

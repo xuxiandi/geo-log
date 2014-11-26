00a8c8e43fec9546ef5182a3b2279153b60edc13
alxponom <alxponom@mail.ru>
2014-11-26 19:29:36 +0000

    An in-place object editing design stage.

--- src/com/geoscope/GeoEye/Space/TypesSystem/TTFVisualization/TTTFVisualizationFunctionality.java
+++ src/com/geoscope/GeoEye/Space/TypesSystem/TTFVisualization/TTTFVisualizationFunctionality.java
@@ -1,9 +1,9 @@
 package com.geoscope.GeoEye.Space.TypesSystem.TTFVisualization;
 
 import com.geoscope.GeoEye.Space.Functionality.TTypeFunctionality;
-import com.geoscope.GeoEye.Space.Functionality.ComponentFunctionality.BaseVisualizationFunctionality.T2DBaseVisualizationFunctionality;
+import com.geoscope.GeoEye.Space.Functionality.ComponentFunctionality.BaseVisualizationFunctionality.TBase2DVisualizationFunctionality;
 
-public class TTTFVisualizationFunctionality extends T2DBaseVisualizationFunctionality {
+public class TTTFVisualizationFunctionality extends TBase2DVisualizationFunctionality {
 
 	public TTTFVisualizationFunctionality(TTypeFunctionality pTypeFunctionality, long pidComponent) {
 		super(pTypeFunctionality,pidComponent);
--- src/com/geoscope/GeoEye/Space/TypesSystem/WMFVisualization/TWMFVisualizationFunctionality.java
+++ src/com/geoscope/GeoEye/Space/TypesSystem/WMFVisualization/TWMFVisualizationFunctionality.java
@@ -1,9 +1,9 @@
 package com.geoscope.GeoEye.Space.TypesSystem.WMFVisualization;
 
 import com.geoscope.GeoEye.Space.Functionality.TTypeFunctionality;
-import com.geoscope.GeoEye.Space.Functionality.ComponentFunctionality.BaseVisualizationFunctionality.T2DBaseVisualizationFunctionality;
+import com.geoscope.GeoEye.Space.Functionality.ComponentFunctionality.BaseVisualizationFunctionality.TBase2DVisualizationFunctionality;
 
-public class TWMFVisualizationFunctionality extends T2DBaseVisualizationFunctionality {
+public class TWMFVisualizationFunctionality extends TBase2DVisualizationFunctionality {
 
 	public TWMFVisualizationFunctionality(TTypeFunctionality pTypeFunctionality, long pidComponent) {
 		super(pTypeFunctionality,pidComponent);

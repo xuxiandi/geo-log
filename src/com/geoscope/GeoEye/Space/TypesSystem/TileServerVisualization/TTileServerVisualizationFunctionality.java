eb151b3ec8f7c60adfdfd5b17ed0e02b9645ccd3
alxponom <alxponom@mail.ru>
2015-01-19 14:26:53 +0000

    Fixes.

--- src/com/geoscope/GeoEye/Space/TypesSystem/TileServerVisualization/TTileServerVisualizationFunctionality.java
+++ src/com/geoscope/GeoEye/Space/TypesSystem/TileServerVisualization/TTileServerVisualizationFunctionality.java
@@ -7,10 +7,10 @@ import java.net.HttpURLConnection;
 import com.geoscope.Classes.IO.Net.TNetworkConnection;
 import com.geoscope.GeoEye.R;
 import com.geoscope.GeoEye.Space.Functionality.TTypeFunctionality;
-import com.geoscope.GeoEye.Space.Functionality.ComponentFunctionality.TComponentFunctionality;
+import com.geoscope.GeoEye.Space.Functionality.ComponentFunctionality.BaseVisualizationFunctionality.TBase2DVisualizationFunctionality;
 import com.geoscope.GeoEye.Space.TypesSystem.TComponentData;
 
-public class TTileServerVisualizationFunctionality extends TComponentFunctionality {
+public class TTileServerVisualizationFunctionality extends TBase2DVisualizationFunctionality {
 
 	public TTileServerVisualizationFunctionality(TTypeFunctionality pTypeFunctionality, long pidComponent) {
 		super(pTypeFunctionality,pidComponent);

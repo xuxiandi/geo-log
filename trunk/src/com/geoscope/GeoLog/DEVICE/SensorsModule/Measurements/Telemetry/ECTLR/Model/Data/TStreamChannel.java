fadc8bd0bc97f42dee8b5bd67b9f26ea5165beba
alxponom@mail.ru <alxponom@mail.ru@8df5ee3e-9bdb-1a15-5823-3a5dad31ce24>
2015-03-24 20:49:39 +0000

    A design stage.

---
+++ trunk/src/com/geoscope/GeoLog/DEVICE/SensorsModule/Measurements/Telemetry/ECTLR/Model/Data/TStreamChannel.java
@@ -0,0 +1,8 @@
+package com.geoscope.GeoLog.DEVICE.SensorsModule.Measurements.Telemetry.ECTLR.Model.Data;
+
+
+public class TStreamChannel extends com.geoscope.GeoLog.DEVICE.SensorsModule.Measurement.Model.Data.TStreamChannel {
+
+	public TStreamChannel() {
+	}
+}

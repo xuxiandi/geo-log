bf5f6d2cc11e328cafa77f9cfcb09fd369b7693f
alxponom <alxponom@mail.ru>
2015-04-10 21:08:41 +0000

    A fix.

--- src/com/geoscope/Classes/Data/Types/Identification/TUIDGenerator.java
+++ src/com/geoscope/Classes/Data/Types/Identification/TUIDGenerator.java
@@ -1,12 +1,12 @@
 package com.geoscope.Classes.Data.Types.Identification;
 
+import java.util.Locale;
 import java.util.UUID;
 
 public class TUIDGenerator {
 
 	public static String Generate() {
 		UUID GUID = UUID.randomUUID();
-		return GUID.toString(); 
+		return GUID.toString().toUpperCase(Locale.ENGLISH); 
 	}
-	
 }
